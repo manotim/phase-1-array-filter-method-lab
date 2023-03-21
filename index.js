ff5e6de3d8c582c2ad3b534a3a2e9a8149f7d4f2
manoti <manotimike@gmail.com>
2023-03-21 23:36:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, attribute) {
+    return drivers.filter((driver) => driver.toLowerCase() === attribute.toLowerCase())
+  }
+
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter((driver) => driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase())
+  }
+
+  function matchName(drivers, argument) {
+    return drivers.filter((driver) => driver.name === argument )
+  }
